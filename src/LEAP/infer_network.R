9b01bd834aa26515fd3b2053c19648fe985c24bc
lmshk <lmshk@users.noreply.github.com>
2021-01-06 16:28:02 +0100

    Clean up LEAP outputs

--- src/LEAP/infer_network.R
+++ src/LEAP/infer_network.R
@@ -2,11 +2,24 @@ arguments <- commandArgs(trailingOnly = TRUE)
 input_filename <- arguments[1]
 output_filename <- arguments[2]
 
+input <- as.matrix(read.csv(input_filename, row.names = 1))
+edges <- LEAP::MAC_counter(input, symmetric = TRUE)
+
+# Map gene indices back to their names when writing.
 write.csv(
-  LEAP::MAC_counter(
-    as.matrix(
-      read.csv(input_filename, row.names = 1)
-    )
+  data.frame(
+    "gene1" = plyr::mapvalues(
+      edges[, "Column gene index"],  # `edges` has class `matrix`
+      1:nrow(input),
+      rownames(input)
+    ),
+    "gene2" = plyr::mapvalues(
+      edges[, "Row gene index"],
+      1:nrow(input),
+      rownames(input)
+    ),
+    "correlation" = edges[, "Correlation"]
   ),
-  file = output_filename
+  file = output_filename,
+  row.names = FALSE
 )
