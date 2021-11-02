e4f20ff416b2036228eeacf519488424711ffe1f
lmshk <lmshk@users.noreply.github.com>
2021-11-02 16:02:28 +0100

    Make Genie3 output loader sort rows and columns by name, like the others

--- src/analysis/load_genie3r_network.function.R
+++ src/analysis/load_genie3r_network.function.R
@@ -1,1 +1,7 @@
-function(filename) as.matrix(read.csv(filename, row.names = 1))
+function(filename) {
+  result = as.matrix(read.csv(filename, row.names = 1))
+
+  # Sort rows and columns by gene name to ensure they are ordered identically.
+  # (They usually are, but we don't have that guaranteed.)
+  result[order(rownames(result)), order(colnames(result))]
+}
